248894da52c0acfc6e6b2427a8927368987dce1c
CriticalGameEror <83310221+CriticalGameEror@users.noreply.github.com>
2022-12-14 09:22:22 +0000

    Day 14 complete

---
+++ day 14/day14part1.py
@@ -0,0 +1,65 @@
+with open("input.txt") as f:
+    input = f.readlines()
+
+for x in range(len(input)):
+    input[x] = input[x].strip()
+    input[x] = input[x].split("->")
+    for y in range(len(input[x])):
+        input[x][y] = input[x][y].split(",")
+    
+locations = {} # [x,y] - key
+boundaries = [10000000000, 0, 0] # lowest x, highest x, highest y
+for rockform in input:
+    for rock in range(0, len(rockform)-1):
+        x = int(rockform[rock][0])
+        y = int(rockform[rock][1])
+        while (x != int(rockform[rock+1][0])) or (y != int(rockform[rock+1][1])):
+            
+            if x < boundaries[0]:
+                boundaries[0] = x
+            if x > boundaries[1]:
+                boundaries[1] = x
+            if y > boundaries[2]:
+                boundaries[2] = y
+
+            if int(rockform[rock+1][0]) < boundaries[0]:
+                boundaries[0] = int(rockform[rock+1][0])
+            if int(rockform[rock+1][0]) > boundaries[1]:
+                boundaries[1] = int(rockform[rock+1][0])
+            if int(rockform[rock+1][1]) > boundaries[2]:
+                boundaries[2] = int(rockform[rock+1][1])
+
+            locations[x,y] = "rock"
+            if x != int(rockform[rock+1][0]):
+                if x < int(rockform[rock+1][0]):
+                    x += 1
+                else:
+                    x -= 1
+            else:
+                if y < int(rockform[rock+1][1]):
+                    y += 1
+                else:
+                    y -= 1
+        locations[x,y] = "rock"
+
+locations[500,0] = "generate"
+
+total = 0
+currentLocation = [500,0]
+while True:
+    if currentLocation[0] < boundaries[0] or currentLocation[0] > boundaries[1] or currentLocation[1] > boundaries[2]:
+        break
+    if (currentLocation[0], currentLocation[1]+1) not in locations:
+        currentLocation = [currentLocation[0], currentLocation[1]+1]
+    elif (currentLocation[0]-1, currentLocation[1]+1) not in locations:
+        currentLocation = [currentLocation[0]-1, currentLocation[1]+1]
+    elif (currentLocation[0]+1, currentLocation[1]+1) not in locations:
+        currentLocation = [currentLocation[0]+1, currentLocation[1]+1]
+    else:
+        locations[currentLocation[0], currentLocation[1]] = "sand"
+        currentLocation = [500,0]
+        total += 1
+print(total)
+
+    
+
